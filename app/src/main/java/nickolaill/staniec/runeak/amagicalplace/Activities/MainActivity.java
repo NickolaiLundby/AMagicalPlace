f61743431a65af925734eac0165f8c4cf692a909
Nickolai Ernst <nickolai.lundby.ernst1@post.au.dk>
2019-04-26 15:47:21 +0200

    Created a blank app

---
+++ app/src/main/java/nickolaill/staniec/runeak/amagicalplace/Activities/MainActivity.java
@@ -0,0 +1,15 @@
+package nickolaill.staniec.runeak.amagicalplace.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import nickolaill.staniec.runeak.amagicalplace.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
