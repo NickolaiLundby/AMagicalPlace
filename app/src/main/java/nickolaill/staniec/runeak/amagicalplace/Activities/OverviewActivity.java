702188176dde4cfa20d9a3b561bdb9586dea3e51
Nickolai Ernst <nickolai.lundby.ernst1@post.au.dk>
2019-05-05 11:51:22 +0200

    Saving instance state for fragments

--- app/src/main/java/nickolaill/staniec/runeak/amagicalplace/Activities/OverviewActivity.java
+++ app/src/main/java/nickolaill/staniec/runeak/amagicalplace/Activities/OverviewActivity.java
@@ -13,9 +13,15 @@ public class OverviewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_overview);
 
-        OverviewFragment fragment = OverviewFragment.newInstance();
-        getSupportFragmentManager().beginTransaction()
-                .replace(R.id.overview_container, fragment)
-                .commit();
+        if (savedInstanceState == null) {
+            OverviewFragment fragment = OverviewFragment.newInstance();
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.overview_container, fragment)
+                    .commit();
+        }
+        else {
+            // If there is a savedInstanceState that means there is a fragment, therefor
+            // we just leave this bracket empty, as this will automatically recreate the fragment(s)
+        }
     }
 }
